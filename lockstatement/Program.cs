5db506b7a38a3a4b09461d655b710ddca1fc7d8b
deviloper <edmundjryan@gmail.com>
2022-05-18 11:39:09 -0400

    commit

---
+++ lockstatement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lockstatement
+{
+    class Lock
+    {
+    }
+
+    class Program
+    {
+        private static Lock myLock = new Lock();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var myLock = new Lock();
+
+            var task1 = Task.Run(() => CallThis(1));
+            var task2 = Task.Run(() => CallThis(2));
+            var task3 = Task.Run(() => CallThis(3));
+
+            while (!task1.IsCompleted || !task2.IsCompleted || !task3.IsCompleted)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        static void CallThis(int callindex)
+        {
+            Console.WriteLine("start CallThis: " + callindex + ", " + DateTime.Now.ToLongTimeString());
+            lock (myLock)
+            {
+                Console.WriteLine("inside lock " + callindex + ", " + DateTime.Now.ToLongTimeString());
+                Thread.Sleep(2000);
+            }
+
+            Console.WriteLine("stop CallThis: " + callindex + " " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
