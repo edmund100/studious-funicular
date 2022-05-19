0047eaec9a3cf7af03ce2e86f86a97197a867eb8
deviloper <edmundjryan@gmail.com>
2022-05-19 11:42:02 -0400

    new files

---
+++ initkeyword/initkeyword/InitExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace initkeyword
+{
+    class InitExample
+    {
+        private double _seconds;
+
+        public double Seconds
+        {
+            get { return _seconds; }
+            init { _seconds = value; }
+        }
+    }
+}
---
+++ initkeyword/initkeyword/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace initkeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("can only assign in initializer");
+            var initExample = new InitExample() { Seconds = 2 };
+
+            Console.WriteLine(initExample.Seconds);
+        }
+    }
+}
