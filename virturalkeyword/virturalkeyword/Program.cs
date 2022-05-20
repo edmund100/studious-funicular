da1dcfd0b1341eb1b3a4f4f43a620b01314dae9b
deviloper <edmundjryan@gmail.com>
2022-05-20 14:26:21 -0400

    new project

---
+++ virturalkeyword/virturalkeyword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace virturalkeyword
+{
+    class MyBaseClass
+    {
+        public virtual void MyFunction()
+        {
+            Console.WriteLine("MyBaseClass:MyFunction");
+            Console.WriteLine("\tYou can override this.");
+        }
+    }
+
+    class MyDerivedClass : MyBaseClass
+    {
+        public override void MyFunction()
+        {
+            Console.WriteLine("MyDerivedClass:MyFunction");
+            base.MyFunction();
+            Console.WriteLine("\tI'm override MyBaseFunction.");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myBaseClass = new MyBaseClass();
+            myBaseClass.MyFunction();
+
+            var myDerivedClass = new MyDerivedClass();
+            myDerivedClass.MyFunction();
+        }
+    }
+}
